261be0f8111832469e1b738b583d563374bd2bea
Philip James <philip@eventbrite.com>
2014-12-18 10:20:28 -0800

    Update script to use absolute location

--- circulate.sh
+++ circulate.sh
@@ -2,4 +2,4 @@
 
 DATE=`TZ=America/Los_Angeles date -d "yesterday 13:00" +%Y-%m-%d`
 cp -a $TWEET_HTML_PATH/index.html $TWEET_HTML_PATH/$DATE.html
-bash generate_html.sh
+bash /home/phildini/pobal/generate_html.sh
